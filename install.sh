39536683b928da8aa7ebb9b1409a2bf70adad00f
Axel_Cht62 <79381645+Axelcht62@users.noreply.github.com>
2023-03-13 21:43:04 +0100

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Verification des droits administrateur
+if [ "$EUID" -ne 0 ]
+then echo "Executer ce script en tant qu'administrateur"
+exit
+fi
+    # Installation de vsftpd
+apt update
+apt install filezilla
+apt install pure-ftpd -y
+    # Configuration de pure-ftpd
+cat <<EOT >> /etc/ftp.conf
+    # Activer le mode anonyme
+anonymous_enable=YES
+    # Interdire l'ecriture en mode anonyme
+anon_upload_enable=NO
+anon_mkdir_write_enable=NO
+    # Limiter l'acces en lecture seule
+anon_world_readable_only=YES
+EOT
+    # Redemarrage du service pure-ftpd
+systemctl restart pure-ftpd
